46771dfe52f515ef72ce66b4ddc56589c0a916b9
dugu007 <dasdurga923@gmail.com>
2022-02-02 23:52:28 +0530

    assignment completed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+const Welcome = (props) => {
+  return (
+    <div className="welcome__container">
+      <h1>Hey {props.name}!</h1>
+      <h2></h2>
+    </div>
+  );
+};
+
+export default Welcome;
